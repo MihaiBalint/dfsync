7e91949d87546f24c2676f61e502510240f981ea
Mihai Balint <balint.mihai@gmail.com>
2020-06-24 12:35:54 +0300

    feat: only monitor file events, discard directory events

--- dfsync/monitor.py
+++ dfsync/monitor.py
@@ -2,7 +2,31 @@ import sys
 import time
 import logging
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileModifiedEvent, FileSystemEventHandler
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class FileChangedEventHandler(FileSystemEventHandler):
+    def on_file_modified(self, event):
+        logging.debug(event)
+
+    def catch_all_handler(self, event):
+        if isinstance(event, FileModifiedEvent):
+            self.on_file_modified(event)
+
+    def on_moved(self, event):
+        self.catch_all_handler(event)
+
+    def on_created(self, event):
+        self.catch_all_handler(event)
+
+    def on_deleted(self, event):
+        self.catch_all_handler(event)
+
+    def on_modified(self, event):
+        self.catch_all_handler(event)
+
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -11,7 +35,7 @@ if __name__ == "__main__":
         datefmt="%Y-%m-%d %H:%M:%S",
     )
     path = sys.argv[1] if len(sys.argv) > 1 else "."
-    event_handler = LoggingEventHandler()
+    event_handler = FileChangedEventHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
