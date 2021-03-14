b23030388cb68339a96a6c775ff1b1f423c26483
Mihai Balint <balint.mihai@gmail.com>
2021-03-14 09:08:01 +0200

    feat: check container status and report nicely; some minor refactoring.

--- dfsync/backends/kube.py
+++ dfsync/backends/kube.py
@@ -13,6 +13,23 @@ class KubeReDeployer:
         # Configs can be set in Configuration class directly or using helper utility
         config.load_kube_config()
         self.api = client.CoreV1Api()
+        self._was_status_printed = {}
+
+    def status(self, image_base):
+        ready_map = {False: "🔴  ", True: "🟢  "}
+        if self._was_status_printed.get(image_base):
+            return
+
+        self._was_status_printed[image_base] = True
+        print("Targeting pods using image: {}".format(image_base))
+        for pod, spec, status in self.generate_matching_containers(image_base):
+            status_msg = status.ready
+            if not status.ready:
+                status_msg = "{} - {}".format(status.ready, status.state.waiting.reason)
+
+            icon = ready_map[status.ready]
+            print("{}{} - ready: {}".format(icon, pod.metadata.name, status_msg))
+        print("")
 
     def split_destination(self, destination):
         split_result = destination.split(":")[:2]
@@ -45,9 +62,7 @@ class KubeReDeployer:
         }
         return " ".join(cmd), env
 
-    def redeploy(self, src_file_path, destination_dir: str = None, **kwargs):
-        image_base, destination_dir = self.split_destination(destination_dir)
-
+    def generate_matching_containers(self, image_base):
         result = self.api.list_pod_for_all_namespaces(watch=False)
         for pod in result.items:
             specs = pod.spec.containers
@@ -56,17 +71,37 @@ class KubeReDeployer:
             for spec, status in zip(specs, statuses):
                 if not status.image.startswith(image_base):
                     continue
+                yield pod, spec, status
 
-                container_dir = self.get_container_destination_dir(
-                    pod, status, destination_dir
+    def redeploy(self, src_file_path, destination_dir: str = None, **kwargs):
+        image_base, destination_dir = self.split_destination(destination_dir)
+        self.status(image_base)
+
+        for pod, spec, status in self.generate_matching_containers(image_base):
+            if not status.ready:
+                print(
+                    "{} will not sync in {}, container isn't ready: {}".format(
+                        src_file_path, pod.metadata.name, status.state.waiting.reason
+                    )
                 )
-                rsh_command, rsh_env = self.get_exec_command(
-                    pod.metadata.namespace, pod.metadata.name, status.name
+                continue
+
+            if not self.dry_run_exec(pod, spec, status):
+                print(
+                    "{} failed to sync in {}".format(src_file_path, pod.metadata.name)
                 )
-                self.sync_files(
-                    rsh_command, src_file_path, container_dir, rsh_env=rsh_env, **kwargs
-                )
-                # self.redeploy_container(pod, spec, status)
+                continue
+
+            container_dir = self.get_container_destination_dir(
+                pod, status, destination_dir
+            )
+            rsh_command, rsh_env = self.get_exec_command(
+                pod.metadata.namespace, pod.metadata.name, status.name
+            )
+            self.sync_files(
+                rsh_command, src_file_path, container_dir, rsh_env=rsh_env, **kwargs
+            )
+            # self.redeploy_container(pod, spec, status)
 
     def sync_files(self, rsh_command, src_file, destination_dir: str = None, **kwargs):
         rsh_destination = ":{}".format(destination_dir)
@@ -77,6 +112,23 @@ class KubeReDeployer:
             blocking_io=True,
             **kwargs
         )
+
+    def dry_run_exec(self, pod, spec, status):
+        try:
+            resp = stream(
+                self.api.connect_get_namespaced_pod_exec,
+                pod.metadata.name,
+                pod.metadata.namespace,
+                container=status.name,
+                command="env",
+                stdin=False,
+                stdout=True,
+                stderr=True,
+                tty=False,
+            )
+            return len(resp) > 0
+        except:
+            return False
 
 
 _instance = KubeReDeployer()
