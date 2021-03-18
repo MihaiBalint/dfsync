48b64a967d50ec182d3606349697e1eaa1370796
Mihai Balint <balint.mihai@gmail.com>
2021-03-18 07:25:39 +0200

    fix: ensure that file events from the monitored dir are always relative to the working dir

--- dfsync/monitor.py
+++ dfsync/monitor.py
@@ -1,3 +1,5 @@
+import os
+import os.path
 import sys
 import time
 import logging
@@ -17,8 +19,12 @@ BACKENDS = {
 }
 
 
+class IgnoreEvent(Exception):
+    pass
+
+
 class FileChangedEventHandler(FileSystemEventHandler):
-    def __init__(self, backend: str = "log", **kwargs):
+    def __init__(self, backend: str = "log", watched_dir: str = ".", **kwargs):
         self.filters = [*ALL_FILTERS]
 
         self.backend = BACKENDS.get(backend)
@@ -26,14 +32,44 @@ class FileChangedEventHandler(FileSystemEventHandler):
             raise ValueError("Backend not found: {}".format(backend))
         self.backend_args = kwargs
         self.raised_exception = False
+        self.abs_watched_dir = os.path.abspath(watched_dir)
 
     def _log_backend(self, event):
         logging.info(event)
 
     def _sync(self, event):
+        # It seems like in some contexts, event.src_path will be an absolute path
+        # and in some other contexts, it will be a relative path
+
         self.backend.sync(
-            src_file_path=event.src_path, event=event, **self.backend_args
+            src_file_path=self._get_path_relative_to_watched_dir(event.src_path),
+            event=event,
+            watched_dir=self.abs_watched_dir,
+            **self.backend_args
         )
+
+    def _get_path_relative_to_watched_dir(self, path):
+        try:
+            abs_path = os.path.abspath(path)
+            common = os.path.commonpath([self.abs_watched_dir, abs_path])
+
+            rel_watched = os.path.relpath(self.abs_watched_dir, start=common)
+            rel_path = os.path.relpath(abs_path, start=common)
+
+            # Sanity checks
+            if rel_watched not in ["", ".", "./"]:
+                raise IgnoreEvent()
+            elif not rel_watched.startswith("."):
+                raise IgnoreEvent()
+
+            # Ensure the dot prefix to the path to make it obvious that this
+            # is a relative path
+            if not rel_path.startswith("."):
+                rel_path = os.path.join(".", rel_path)
+
+            return rel_path
+        except ValueError as e:
+            raise IgnoreEvent() from e
 
     def _propagate_event(self, event):
         for file_filter in self.filters:
@@ -44,6 +80,8 @@ class FileChangedEventHandler(FileSystemEventHandler):
     def catch_all_handler(self, event):
         try:
             self._propagate_event(event)
+        except IgnoreEvent:
+            pass
         except:
             self.raised_exception = True
             raise
@@ -75,7 +113,7 @@ def split_destination(destination):
         return "rsync", destination
 
 
-if __name__ == "__main__":
+def main():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(message)s",
@@ -95,14 +133,15 @@ if __name__ == "__main__":
 
     backend, destination_dir = split_destination(destination_dir)
     print("Destination, {}: '{}'".format(backend, destination_dir))
-    print("Watching dir: '{}', press [Ctrl-C] to exit\n".format(path))
-    event_handler = FileChangedEventHandler(backend, destination_dir=destination_dir)
+    event_handler = FileChangedEventHandler(
+        backend, destination_dir=destination_dir, watched_dir=path
+    )
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-
     try:
         event_handler.on_monitor_start()
+        print("Watching dir: '{}', press [Ctrl-C] to exit\n".format(path))
+        observer.start()
         while event_handler.raised_exception is False:
             time.sleep(0.2)
     except KeyboardInterrupt:
@@ -111,3 +150,7 @@ if __name__ == "__main__":
         observer.stop()
         event_handler.on_monitor_exit()
         observer.join()
+
+
+if __name__ == "__main__":
+    main()
